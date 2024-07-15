c3c9900c0315addf2be8b4d9fbe4f126e4073c5e
hirdeshmewada <djhd221@gmail.com>
2024-07-15 15:58:44 +0530

    authenticatiom

--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input"
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input
 }
